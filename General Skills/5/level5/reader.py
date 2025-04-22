23bfe528990f5b8c5d0da202fd9d106a23ec8924
YASH <yashchaurasia630@gmail.com>
2025-04-22 20:43:57 +0530

    fix

--- General Skills/5/level5/reader.py
+++
@@ -1,9 +0,0 @@
-passwords = []
-
-with open('dictionary.txt', 'rb') as f:
-    for lines in f:
-        pw = str(f.readline())
-        passwords.append(pw[2:6])
-        
-print(passwords)
-print(len(passwords))
--- General Skills/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssh -p 56271 picoplayer@saturn.picoctf.net
-#echo "yes"
-echo "ENAFb6zfzn"
-cd /
-cd challenge
-ls
-cat metadata.json
